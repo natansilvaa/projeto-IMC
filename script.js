ffee7fc58dd0964e4710abd2b54b8a5ca6845941
natansilvaa <natan.picanco24@gmail.com>
2025-03-27 14:28:28 -0300

    comecei a codar o js

---
+++ script.js
@@ -0,0 +1,14 @@
+const nome = document.getElementById('nome');
+
+const altura = document.getElementById('altuta');
+
+const peso = document.getElementById('peso');
+
+const calcular = document.getElementById('calcualar');
+
+const resultado = document.getElementById('resultado');
+
+
+
+
+calcular.addEventListener( 'click, imc')
