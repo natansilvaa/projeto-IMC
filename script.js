523682b0dcd67390211bbac7bce2c9d0ad4ab227
natansilvaa <natan.picanco24@gmail.com>
2025-03-28 14:26:24 -0300

    ajustei o js

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ function imc () {
         if ( valorIMC <18.5){
            classificacao ='abaixo do peso'
         }else if (valorIMC <25){
-            classificacao = 'com o peso ideal.'
+            classificacao = 'com o peso ideal, Parabéns!!!'
         }else if( valorIMC <35){
             classificacao = ' com obesidade grau I'
         }else if(valorIMC < 40){
             classificacao ='com obesidade grau II'
         }else {
-            classificacao ='com obesidade vinel III'
+            classificacao ='com obesidade grau III , cuidado!!!'
         }
 
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
