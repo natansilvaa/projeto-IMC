0470953b1f09c3fe6f6015626e9f05b0b6b316b5
natansilvaa <natan.picanco24@gmail.com>
2025-03-28 01:47:53 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const nome = document.getElementById('nome');
 
-const altura = document.getElementById('altuta');
 
-const peso = document.getElementById('peso');
-
-const calcular = document.getElementById('calcualar');
-
-const resultado = document.getElementById('resultado');
+const calcular = document.getElementById('calcular');
 
 
 
 
-calcular.addEventListener( 'click, imc')
+function imc () {
+
+    const nome = document.getElementById('nome').value;
+
+    const altura = document.getElementById('altura').value;
+
+    const peso = document.getElementById('peso').value;
+
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== ''){
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if ( valorIMC <18.5){
+           classificacao ='abaixo do peso'
+        }else if (valorIMC <25){
+            classificacao = 'com o peso ideal.'
+        }else if( valorIMC <35){
+            classificacao = ' com obesidade grau I'
+        }else if(valorIMC < 40){
+            classificacao ='com obesidade grau II'
+        }else {
+            classificacao ='com obesidade vinel III'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+
+    }else{
+        resultado.textContent='Preencha todos os campos!!!'
+    }
+}
+
+calcular.addEventListener('click', imc)
